ea300f461b0a79975813a9cd7646e745aad1affd
30parth <aparth3010@gmail.com>
2026-03-15 11:14:00 +0530

    update routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::livewire('/post', 'post.index');
+Route::livewire('/post/create', 'post.form');
